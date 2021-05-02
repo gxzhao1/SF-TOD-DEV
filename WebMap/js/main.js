69df6289b1a53aa2264b89dd7d2f0e5110e09ecf
gxzhao1 <70733920+gxzhao1@users.noreply.github.com>
2021-05-02 03:03:01 -0500

    Update search filters
    Updated turf.js to a newer version. However, the buffer wouldn't work and everything seems correct

--- WebMap/js/main.js
+++ WebMap/js/main.js
@@ -14,8 +14,37 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
 
 /* Global Vars */
 var stationDataset = "https://raw.githubusercontent.com/gxzhao1/SF-TOD-DEV/main/Data/BART_Station.geojson";
+<<<<<<< Updated upstream
 var propertyDataset = "https://data.sfgov.org/resource/wv5m-vpq2.json?closed_roll_year=2019&property_class_code=D";
 var inputValue;
+=======
+var propertyDataset = "https://data.sfgov.org/resource/wv5m-vpq2.json";
+var inputValue;
+var filteredProperty;
+var previousmarkers=[];
+var currentmarkers=[];
+var Buffer1=[];
+var Buffer2=[];
+var Buffer1Property;
+var propertyfeaturecollection;
+var Buffer1featurecollection;
+var Buffer2featurecollection;
+var Bufferarray=[];
+var ptsArray=[];
+
+$("#yearRange").on("click", function(e) { //for year range slider
+  year = $('#yearRange').val()
+  $(".year").text(year);
+ });
+
+var customOptions = //for popup
+  {
+  'maxWidth': '400',
+  'width': '200',
+  'className' : 'popupCustom'
+  }
+
+>>>>>>> Stashed changes
 
 var legend = L.control({ position: "bottomleft" });
 legend.onAdd = function(map) {
@@ -45,8 +74,13 @@ function plotStationMarker(data) {
 
   var markerOptions = { icon: customIcon }; 
 
+<<<<<<< Updated upstream
   return L.marker([a.geometry.coordinates[1],a.geometry.coordinates[0]],
       markerOptions 
+=======
+  return L.marker([a.geometry.coordinates[1], a.geometry.coordinates[0]],
+      markerOptions
+>>>>>>> Stashed changes
       ).addTo(map).bindPopup(
           "Name: " + a.properties.Name +
           "<br>Address: " + a.properties.Street +
@@ -54,6 +88,7 @@ function plotStationMarker(data) {
       })
 }
 
+<<<<<<< Updated upstream
 function plotStationBuffer(data) {
   data.map(function(a) {
     if (a.properties.City == "San Francisco") {
@@ -78,10 +113,77 @@ function plotPropertyMarker(data) {
             "<br>Address: " + a.properties.Street +
             "<br>Link: " + a.properties.Link) */
     }
+=======
+function makeBuffer1 (data){
+  return data.map(function(a){
+    if(a.properties.City == "San Francisco"){    /* Not Working */
+      var stationPoint = turf.point([a.geometry.coordinates[1], a.geometry.coordinates[0]]);
+      var stationBuffer1 = turf.buffer(stationPoint, 0.5, {units: 'miles'});
+      return L.polygon(stationBuffer1.geometry.coordinates[0], {color: '#977f8c'})
+    } 
+  })
+}
+
+function makeBuffer2 (data){
+  return data.map(function(a){
+    if(a.properties.City == "San Francisco"){
+      var stationPoint = turf.point([a.geometry.coordinates[1],a.geometry.coordinates[0]]);
+      var stationBuffer2 = turf.buffer(stationPoint, 1, {units: 'miles'});
+      return L.polygon(stationBuffer2.geometry.coordinates[0], {color: '#d0bcca'})
+    }
+
   })
 }
 
 
+function plotStationBuffer(buffer) {
+  _.each(buffer,function(a){
+    console.log(a)
+    a.addTo(map)
+  })
+}
+
+
+
+var makeMarkers = function(data) {
+    return data.map(function(a){
+    if (Object.keys(a).includes("the_geom")==true) {
+      var customIcon = L.divIcon({className: "propertyPoint"});
+      var markerOptions = { icon: customIcon };
+      leafletMarker = L.marker([a.the_geom.coordinates[1], a.the_geom.coordinates[0]], markerOptions).bindPopup("Neighborhood: " + a.assessor_neighborhood +
+      "<br>Address: " + a.property_location +
+      "<br>Year Built: " + a.year_property_built+
+      "<br>Use: " +a.use_definition+
+      "<br>Personal Property Value: "+a.assessed_personal_property_value+
+      "<br>Land Value: " + a.assessed_land_value +
+      "<br>Improvement Value: " +a. assessed_improvement_value, 
+      customOptions )
+      console.log(leafletMarker)
+      return leafletMarker
+    }
+  })
+}
+
+
+function plotPropertyMarker(marker) {
+  _.each(marker,function(a){
+    a.addTo(map)
+>>>>>>> Stashed changes
+  })
+}
+
+function prepPropPts(data) {
+  data.map(function(a) {
+    var coords = [a.the_geom.coordinates[1], a.the_geom.coordinates[0]];
+    var yearBuilt = a.year_property_built;
+    var yearBuiltObj =  {"Year Built": yearBuilt};
+    var pt = turf.point(coords, yearBuiltObj);
+    ptsArray.push(pt)
+  })
+}
+
+
+<<<<<<< Updated upstream
 /* ===================== Main Process ===================== */
 $.when(
   $.ajax(stationDataset), 
@@ -98,11 +200,56 @@ $.when(
 
   
 
+=======
+ var resetMap = function (){
+   _.each(previousmarkers,function(marker,i){
+     map.removeLayer(marker);
+   })
+   previousmarkers=[];
+ }
+
+
+
+/* ===================== Main Process ===================== */
+$.when(
+  $.ajax({
+    url: stationDataset,
+    type:"GET",
+    data: {
+      format: 'geojson'
+    }
+  }),
+).then(function(station) {
+  //* All about stations *//
+  stationData = JSON.parse(station).features;
+  plotStationMarker(stationData);
+  Buffer1 = makeBuffer1(stationData);
+  Buffer1 = Buffer1.filter(function(x){
+    return x!==undefined
+  })
+  console.log("Buffer1", Buffer1)
+  // bufferarray(Buffer1);
+  Buffer2 = makeBuffer2(stationData);
+  Buffer2 = Buffer2.filter(function(x){
+    return x!==undefined
+  })
+  console.log("Buffer2", Buffer2)
+  plotStationBuffer(Buffer1);
+  plotStationBuffer(Buffer2);
+
+  var Buffer1feature = Buffer1.map(function(layer){return layer.toGeoJSON()});
+  Buffer1featurecollection = turf.featureCollection(Buffer1feature);
+  var Buffer2feature = Buffer2.map(function(layer){return layer.toGeoJSON()});
+  Buffer2featurecollection = turf.featureCollection (Buffer2feature);
+
+  //* sidebar interactions *//
+>>>>>>> Stashed changes
   $('#sidebarCollapse').on('click', function (e) {
     $('#sidebar').toggleClass('active');
     $('#map').toggleClass('active');
   });
 
+<<<<<<< Updated upstream
   $("#searchButton").on("click", function(e) {
     const inputValue = $('#searchInput').val();
     console.log(inputValue);  /* I could not make inputValue a global variable or plot within this click event! Console log doesn't work but alert() does/ */
@@ -111,5 +258,77 @@ $.when(
   /* These don't work yet */
   filteredProperty = propertyData.filter(a => a.assessor_neighborhood.toLowerCase() == inputValue);
   plotPropertyMarker(filteredProperty);
+=======
+  //* search interactions *//
+  $("#searchButton").on("click", function(e) {
+    resetMap()
+    yearValue= $('#yearRange').val();
+    neighValue= $('#neighInput').val().replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()); // this capitalize the first letter of each word
+    proptValue= $('#proptInput').val();
+    if (neighValue == "") {
+      filteredDataset = propertyDataset + "?" + "closed_roll_year=" + yearValue + "&use_definition=" + proptValue;
+    } else {
+      filteredDataset = propertyDataset + "?" + "closed_roll_year=" + yearValue + "&use_definition=" + proptValue + "&assessor_neighborhood=" + neighValue;
+    }
+    //* Get filtered property value *//
+    $.ajax({
+      type:"GET",
+      url: filteredDataset, //?closed_roll_year=2019&property_class_code=D
+      data: {
+        "$limit" : 1000000,
+        "$$app_token" : "1tkBmJ7LlU1rL4drYdWaz9Ytr"
+      }
+    }).done(function(property) {
+      currentmarkers = makeMarkers(property);
+      console.log(filteredDataset, property)
+      //map.flyTo(currentmarkers[0]._latlng)
+      //map.flyTo(currentmarkers[0])
+      plotPropertyMarker(currentmarkers)
+      prepPropPts(property);
+      var pointFC = turf.featureCollection(ptsArray);
+      console.log("ptsfc", pointFC)
+      var collected = turf.collect(pointFC, Buffer1featurecollection, 'Year Built', 'Value');
+      console.log("collected", collected); // doesn't work right here
+      var values = collected.features[0].properties.values
+      console.log("values", values)
+
+      //* prepare lookup table *//
+      ///*Convert to multipolygon feature *///
+      var polygon = Buffer1[2].toGeoJSON();
+      console.log(polygon)
+      console.log("points array", ptsArray)
+
+
+    })
+      
+/*     filteredProperty = propertyData.filter(a => a.assessor_neighborhood === inputValue) 
+    console.log(filteredProperty)
+    currentmarkers = makeMarkers (filteredProperty)
+    currentmarkers=currentmarkers.filter(function(x){
+      return x !== undefined
+    })
+
+    console.log(currentmarkers) */
+    /* Write a function to filter the currentmarkers*/
+    // pointclassify(currentmarkers,Buffer1);  ....function
+    /* test work, function does not work*/
+    // var propertyfeature = currentmarkers[1].toGeoJSON();
+    // var polygon = Buffer1[2].toGeoJSON();
+    // var features = {"type": "FeatureCollection","features": [propertyfeature,polygon]}
+    // console.log(features)
+    // console.log(turf.inside(propertyfeature,polygon))  /* Inside Work*/
+    // var propertyfeature = currentmarkers.map(function(point){return point.toGeoJSON()});
+    /*........inside needs freature instead feature collection..... */
+/*     var propertyfeature = currentmarkers.map(function(a){ return a.toGeoJSON()})
+    propertyfeaturecollection = turf.featureCollection(propertyfeature);
+    console.log(propertyfeature)
+    console.log(propertyfeaturecollection)
+    plotPropertyMarker(currentmarkers)
+
+    return(inputValue,filteredProperty,propertyfeaturecollection); */
+  })  ;
+
+
+>>>>>>> Stashed changes
 
 })
